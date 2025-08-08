1539ce056d2aa1866841852b20666aff929e74f2
Desmond Selorm <psgquanas@gmail.com>
2025-08-08 16:58:35 +0000

    First Commit

---
+++ config/inngest.js
@@ -0,0 +1,67 @@
+import { Inngest } from "inngest";
+import dbConnect from "./db";
+import User from "@/models/user";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart-next" });
+
+// inngest func to save user data to db
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: "sync-user-from-clerk"
+    },
+    {
+        event: "clerk/user.created"
+    },
+
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            imageUrl: image_url,
+        }
+        await dbConnect()
+        await User.create(userData);
+    }
+)
+
+// inngest func to update user data in db
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: "update-user-from-clerk"
+    },
+    {
+        event: "clerk/user.updated"
+    },
+
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            imageUrl: image_url,
+        }
+        await dbConnect()
+        await User.findByIdAndUpdate(id, userData);
+    }
+)
+
+// inngest func to delete user data from db
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: "delete-user-with-clerk"
+    },
+    {
+        event: "clerk/user.deleted"
+    },
+
+    async ({event}) => {
+        const {id} = event.data;
+
+        await dbConnect()
+        await User.findByIdAndDelete(id);
+    }
+)
